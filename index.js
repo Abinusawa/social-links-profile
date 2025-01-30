f74b8b4801f81b6b186db329535157ebc3ce8f79
OpeDavid <aopeyemi906@gmail.com>
2025-01-30 14:58:18 +0100

    hng links profile

---
+++ index.js
@@ -0,0 +1,8 @@
+function updateUTCTime() {
+    const utcTimeElement = document.getElementById("utc-time");
+    const now = new Date();
+    utcTimeElement.textContent = now.toUTCString();
+}
+
+// Update the time when the page loads
+updateUTCTime();
